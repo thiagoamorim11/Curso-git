d7c7f1a53502a636fde213b96538de60356631d1
Thiago Amorim <ttthiago.amorim@gmail.com>
2021-02-03 11:32:44 -0300

    projeto teste iniciado

---
+++ Projeto_Teste/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projeto_Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");
+        }
+    }
+}
